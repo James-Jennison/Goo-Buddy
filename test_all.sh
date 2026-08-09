2db2943966ecad4fef52b467ad450bf409cdadb8
codeframe78 <codeframe@gmail.com>
2026-08-09 05:48:04 -0700

    ci: fail fast on dependency advisories

--- test_all.sh
+++ test_all.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./test_frontend.sh && ./test_backend.sh --full && ./test_docker.sh && ./test_security.sh --full
+# Run the fast dependency audit before the longer frontend, backend, and Docker
+# gates so known advisories fail immediately instead of after a full test run.
+./test_security.sh --full && ./test_frontend.sh && ./test_backend.sh --full && ./test_docker.sh
